99d300ee4f3017544e8f82260d077bec6cd6b176
Surya Bhai <jsurya352@gmail.com>
2017-06-20 19:02:03 +0530

    code fix for app popup func of client tab page

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -9,15 +9,25 @@ import {Router} from "@angular/router";
 import {User} from "../../models/user";
 import { BootstrapModalModule } from 'ng2-bootstrap-modal';
 import { ConfirmComponent } from '../confirmbox/confirm.component';
-import { DialogService } from "ng2-bootstrap-modal";
+import { DialogService, DialogComponent} from "ng2-bootstrap-modal";
 import {MenuComponent} from "../menu/menu.component";
+
+
+export interface ConfirmModel {
+    title: string;
+    message: string;
+    addNewClient: boolean;
+    getClientsData: boolean;
+
+}
+
 
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
   styleUrls: ['./clients.component.sass']
 })
-export class ClientsComponent implements OnInit {
+export class ClientsComponent extends DialogComponent<ConfirmModel, boolean> implements OnInit {
 
     private clientList: client[];
     public addClient: FormGroup; // our model driven form
@@ -27,6 +37,9 @@ export class ClientsComponent implements OnInit {
     private user: User;
     private deleteclients: any;
     private clientName: any;
+    public addNewClient: boolean;
+    public getClientsData: boolean = true;
+    public newclitem: any = {};
 
     public DefaultResponse = {"status": "Active" };
 
@@ -86,21 +99,41 @@ export class ClientsComponent implements OnInit {
             perPage: 10
         }
     };
+    //source: LocalDataSource = new LocalDataSource();
+    constructor(private clientService: ClientsService, private appService: AppService, private router: Router, public dialogService: DialogService, private menuComponent: MenuComponent) {
+        super(dialogService); 
+        //this.addClient = new FormGroup({
+        //    firstName: new FormControl(''),
+        //    lastName: new FormControl(''),
+        //    email: new FormControl(''),
+        //    phone: new FormControl(''),
+        //    statusId: new FormControl('')
+
+        //});
+    }
     source: LocalDataSource = new LocalDataSource();
-    constructor(private clientService: ClientsService, private appservice: AppService, private router: Router, private dialogService: DialogService, private menuComponent: MenuComponent) {
-        this.addClient = new FormGroup({
-            firstName: new FormControl(''),
-            lastName: new FormControl(''),
-            email: new FormControl(''),
-            phone: new FormControl(''),
-            statusId: new FormControl('')
+    getCliData() {
+        this.appService.showSideBarMenu(null, "clients");
+        this.clientService.getClients(this.appService.orgId).subscribe((res) => {
+            this.clientList = res['clients'];
+            this.clientList.forEach(client => {
+                if (client.markForDeletion)
+                    client.status = 'Mark for Deletion';
+            });
+            this.source.load(this.clientList);
 
-        });  }
-
-  ngOnInit() {
-    this.appservice.showSideBarMenu(null, "clients");
+            if (this.appService.user) {
+                this.user = this.appService.user;
+            }
+            });
+    }
+    //ngOnInit() {
+    //    this.getCliData();
+    //}
+      ngOnInit() {
+    this.appService.showSideBarMenu(null, "clients");
     this.clientService
-      .getClients(this.appservice.orgId)
+      .getClients(this.appService.orgId)
       .subscribe((res: any) => {
         this.clientList = res.clients;
         this.clientList.forEach(client => {if(client.markForDeletion)
@@ -111,17 +144,50 @@ export class ClientsComponent implements OnInit {
         console.log(this.data);
         console.log(this.clientList);
         });
-    if (this.appservice.user) {
-        this.user = this.appservice.user;
+    if (this.appService.user) {
+        this.user = this.appService.user;
     }
 
 
   }
+    addNewCli() {
+        this.dialogService.addDialog(ClientsComponent, {
+            addNewClient: true,
+            getClientsData: false,
+            title: 'Add Client',
+        }).subscribe((isConfirmed) => {
+            if (isConfirmed) {
+                this.clientService.saveNewClient(this.appService.newclitem).subscribe((res) => {
+                    if (res['statusCode'] == 'SUCCESS') {
+                        this.getCliData();
+                    }
+                });
+            }
+        });
+    }
+    clientSave() {
+        this.newclitem['accountId'] = this.appService.user.accountId;
+        this.newclitem['orgId'] = this.appService.orgId;
+        this.newclitem['createdBy'] = this.appService.user.userId;
+        if (this.newclitem['status'] == '' || null || undefined) {
+            this.newclitem['status'] = "Active";
+        }
+        this.appService.newclitem = this.newclitem;
+        this.result = true;
+        this.close();
+    }
+    cancel() {
+        this.result = false;
+        this.close();
+    }
+
+
+
 
   onCreateConfirm(event) : void {
-      event.newData['accountId'] = this.appservice.user.accountId;
-      event.newData['orgId'] = this.appservice.orgId;
-      event.newData['createdBy'] = this.appservice.user.userId;
+      event.newData['accountId'] = this.appService.user.accountId;
+      event.newData['orgId'] = this.appService.orgId;
+      event.newData['createdBy'] = this.appService.user.userId;
 
       if (event.newData['status'] == '' || null || undefined) {
           event.newData['status'] = "Active";
@@ -141,7 +207,7 @@ export class ClientsComponent implements OnInit {
   }
 
   onEditConfirm(event) : void {
-    this.clientService.updateClient(event.newData, this.appservice.user.userId).subscribe((res) => {
+    this.clientService.updateClient(event.newData, this.appService.user.userId).subscribe((res) => {
               this.message = res['statusCode'];
               if(this.message === "SUCCESS"){
                 event.confirm.resolve(event.newData);
@@ -164,7 +230,7 @@ export class ClientsComponent implements OnInit {
               //Get dialog result
               //this.confirmResult = isConfirmed;
               if (isConfirmed) {
-                  this.clientService.removeclient(clients.data['clientId'], this.appservice.user.userId).subscribe((res) => {
+                  this.clientService.removeclient(clients.data['clientId'], this.appService.user.userId).subscribe((res) => {
                       this.message = res['statusCode'];
                       clients.data.clientStatus = "Mark for Deletion";
                       clients.confirm.reject();
@@ -176,8 +242,8 @@ export class ClientsComponent implements OnInit {
   }
   onUserRowClick(event): void{
       this.menuComponent.highlightSBLink('Client Details');
-    this.appservice.moveToPage("immigrationview-client-details");
-    this.appservice.clientId = event.data.clientId;
+    this.appService.moveToPage("immigrationview-client-details");
+    this.appService.clientId = event.data.clientId;
       
   }
 
