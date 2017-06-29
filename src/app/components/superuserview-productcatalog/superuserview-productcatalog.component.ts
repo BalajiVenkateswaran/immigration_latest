e482cab0ad319282dfe4b0d186f89edf60b1a64d
balaji <balaji@PTGHYD>
2017-06-29 19:08:35 +0530

    Build Issue fix

--- src/app/components/superuserview-productcatalog/superuserview-productcatalog.component.ts
+++ src/app/components/superuserview-productcatalog/superuserview-productcatalog.component.ts
@@ -104,6 +104,86 @@ export class SuperuserviewProductcatalogComponent implements OnInit {
     this.appService.showSideBarMenu("superuserview-product", "superuserview-productcatalog");
     this.source.load(this.products);
   }
+  onCreateConfirm(event): void {
+      //console.log("User table onCreateConfirm event: %o", event.newData);
+      //event.newData['role'] = this.roles[event.newData['roleName']];
+      //event.newData['accountId'] = this.appService.user.accountId;
+      //this.manageAccountUserService.saveNewUser(event.newData).subscribe((res) => {
+      //    this.message = res['statusCode'];
+      //    event.newData['userId'] = res['userId'];
+      //    if (this.message == "SUCCESS") {
+      //        event.confirm.resolve(event.newData);
+      //    } else {
+      //        this.dialogService.addDialog(ConfirmComponent, {
+      //            title: 'Error..!',
+      //            message: 'Unable to Add User..!'
+      //        });
+      //        event.confirm.reject();
+      //    }
+      //});
+  }
+
+  onEditConfirm(event): void {
+      //console.log("User table onEditConfirm event: %o",event.newData);
+      //event.newData['role'] = this.roles[event.newData['roleName']];
+      //this.manageAccountUserService.updateUser(event.newData).subscribe((res) => {
+      //      this.message = res['statusCode'];
+      //      if(this.message === "SUCCESS"){
+      //        event.confirm.resolve(event.newData);
+      //      } else {
+      //          this.dialogService.addDialog(ConfirmComponent, {
+      //              title: 'Error..!',
+      //              message: 'Unable to Edit User..!'
+      //          });
+      //        event.confirm.resolve(event.data);
+      //      }
+      //});
+      //this.editFlag = true;
+      //if (this.editFlag) {
+      //    this.beforeEdit = (<any>Object).assign({}, event.data);
+      //}
+      //this.dialogService.addDialog(ManageAccountUserComponent, {
+      //    adduser: true,
+      //    getUsers: false,
+      //    title: 'Edit User',
+      //    addUsers: this.editFlag ? this.beforeEdit : this.addUsers,
+
+
+      //}).subscribe((isConfirmed) => {
+      //    if (isConfirmed) {
+      //        this.manageAccountUserService.updateUser(this.appService.addUsers).subscribe((res) => {
+      //            if (res['statusCode'] == 'SUCCESS') {
+      //                this.getManageUsers();
+      //            }
+      //        });
+      //    }
+      //    else {
+      //        this.editFlag = false;
+      //    }
+      //});
+  }
+
+  onDeleteConfirm(event): void {
+      //this.dialogService.addDialog(ConfirmComponent, {
+      //    title: 'Confirmation',
+      //    message: 'Are you sure you want to Delete ' + event.data['emailId'] + '?'
+      //})
+      //    .subscribe((isConfirmed) => {
+      //        if (isConfirmed) {
+      //            this.manageAccountUserService.deleteUser(event.data['userId'], this.appService.user.accountId).subscribe((res) => {
+      //                //this.message = res['statusCode'];
+      //                //if (this.message == 'SUCCESS') {
+      //                //    event.confirm.resolve();
+      //                //} else {
+      //                //    event.confirm.reject();
+      //                //}
+      //                if (res['statusCode'] == 'SUCCESS') {
+      //                    this.getManageUsers();
+      //                }
+      //            });
+      //        }
+      //    });
+  }
  
 }
 
