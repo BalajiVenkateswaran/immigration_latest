aac6472c37106c479f1686583a302fcff96f4991
vishnutelukutla <vishnutelukutla@gmail.com>
2017-07-04 18:51:47 +0530

    account preferences

--- src/app/components/superuserview-accountpreferences/accountpreferences.service.ts
+++ src/app/components/superuserview-accountpreferences/accountpreferences.service.ts
@@ -2,4 +2,27 @@
 import {RestService} from "../../services/rest.service";
 
 @Injectable()
-export class SuperuserViewAccountpreferencessService { }
+export class SuperuserViewAccountpreferencessService {
+ constructor(private restService: RestService) {
+    }
+ public getproductsAccount(accountid: string) {
+     return this.restService.getData("/superuser/account/" + accountid + "/products");
+ }
+ public getdiscountsAccount(accountid: string) {
+     return this.restService.getData("/superuser/account/" + accountid + "/discounts");
+ }
+ public saveproduct(addproduct: any,accountid:string) {
+     var req = {
+         "accountId": accountid,
+         "products": [addproduct]
+     }
+     return this.restService.postData("/superuser/accounts/products", req);
+ }
+ public savediscount(adddiscount: any,accountid:string) {
+     var req = {
+         "accountId": accountid,
+         "discounts": [adddiscount]
+     }
+     return this.restService.postData("/superuser/accounts/discounts", req);
+ }
+ }
