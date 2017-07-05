a9becd3142e868c9b8c682b17b55751137417809
vishnutelukutla <vishnutelukutla@gmail.com>
2017-07-05 15:35:02 +0530

    userroles popup

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,13 +7,20 @@ import {ManageAccountUserService} from "../manageaccount-user/manageaccount-user
 import {AppService} from "../../services/app.service";
 import {loginService} from "./login.service";
 import { ConfirmComponent } from '../confirmbox/confirm.component';
-import { DialogService } from "ng2-bootstrap-modal";
+import { DialogService, DialogComponent} from "ng2-bootstrap-modal";
 
+export interface ConfirmModel {
+    title: string;
+    message: string;
+    getloginpage: boolean;
+    selectrole: boolean;
+    userRoles: any;
+}
 @Component({
   selector: 'imm-login',
   templateUrl: 'login.component.html'
 })
-export class LoginComponent implements  OnInit {
+export class LoginComponent extends DialogComponent< ConfirmModel, boolean > implements  OnInit {
 
   private outlet: any = {
     breadcrumbs: null,
@@ -28,14 +35,17 @@ export class LoginComponent implements  OnInit {
   message: string;
   forgotPwd;
   private frgtEmail;
-
+  public getloginpage: boolean = true;
+  public selectrole: boolean;
+  public userRoles: any = [];
   constructor(
     private router: Router,
     private ManageAccountUserService: ManageAccountUserService,
     private appService: AppService,
     private loginservice: loginService,
-    private dialogService: DialogService
+    public dialogService: DialogService
   ) {
+      super(dialogService);
     this.login = new FormGroup({
       emailId: new FormControl(''),
       password: new FormControl('')
@@ -86,39 +96,57 @@ export class LoginComponent implements  OnInit {
 
   loginSubmit(model: User, isValid: boolean) {
     if (isValid) {
-      this.ManageAccountUserService.login(model).subscribe((res: any) => {
-        if(res.statusCode=="FAILURE") {
-          this.message = res.statusDescription;
-        } else {
-          this.appService.user = res.user;
-          this.appService.organizations = res.organizationList;
+        this.ManageAccountUserService.login(model).subscribe((res: any) => {          
+            console.log("Login User %o", res);        
+            if (res.statusCode == "FAILURE") {
+                this.message = res.statusDescription;
+            } else {               
+                if (res.hasMultipleRoles == true) {
+                    
+                    this.dialogService.addDialog(LoginComponent, {
+                        selectrole: true,
+                        getloginpage: false,
+                        title: 'Please Select User Role',
+                        userRoles: res['userAccountRoleList'],
+                    }).subscribe((isConfirmed) => {
+                        if (isConfirmed) {
+                  
 
-          //Reset password
-          if(res['resetPassword'] != undefined
-          && res['resetPassword'] == true){
-            this.appService.moveToPage('reset-password');
-          } else {
-            if (res.userAccountRoleList[0].roleName == "IMMIGRATION MANAGER"
-            || res.userAccountRoleList[0].roleName == "IMMIGRATION OFFICER") {
-                this.appService.applicationViewMode = "Immigration";
-                this.appService.orgId = res.organizationList[0].orgId;
-                this.appService.currentTab = 'petitions';
-                this.appService.moveToPage("petitions");
+                        }
+                        });
+                    this.appService.user = res.user;
+                }
+                if (res.hasMultipleRoles == false) {
+                    this.appService.user = res.user;
+                    this.appService.organizations = res.organizationList;
+                    this.appService.user['roleName'] = res.userAccountRoleList[0].roleName;
+                //Reset password
+                if (res['resetPassword'] != undefined
+                    && res['resetPassword'] == true) {
+                    this.appService.moveToPage('reset-password');
+                } else {
+                    if (res.userAccountRoleList[0].roleName == "IMMIGRATION MANAGER"
+                        || res.userAccountRoleList[0].roleName == "IMMIGRATION OFFICER") {
+                        this.appService.applicationViewMode = "Immigration";
+                        this.appService.orgId = res.organizationList[0].orgId;
+                        this.appService.currentTab = 'petitions';
+                        this.appService.moveToPage("petitions");
+                    }
+                    if (res.userAccountRoleList[0].roleName == "CLIENT") {
+                        this.appService.applicationViewMode = "Client";
+                        this.appService.clientId = this.appService.user.userId;
+                        this.appService.currentTab = 'clientview-petitions';
+                        this.appService.moveToPage("clientview-petitions");
+                    }
+                    // To Do
+                    //if (res.userAccountRoleList[0].roleName == "SUPERUSER") {
+                    //    this.appService.applicationViewMode = "Superuser";
+                    //    this.appService.clientId = this.appService.user.userId;
+                    //    this.appService.currentTab = 'accounts';
+                    //    this.appService.moveToPage("superuser-accounts");
+                    //}
+                }
             }
-            if (res.userAccountRoleList[0].roleName == "CLIENT") {
-                this.appService.applicationViewMode = "Client";
-                this.appService.clientId = this.appService.user.userId;
-                this.appService.currentTab = 'clientview-petitions';
-                this.appService.moveToPage("clientview-petitions");
-            }
-              // To Do
-            //if (res.userAccountRoleList[0].roleName == "SUPERUSER") {
-            //    this.appService.applicationViewMode = "Superuser";
-            //    this.appService.clientId = this.appService.user.userId;
-            //    this.appService.currentTab = 'accounts';
-            //    this.appService.moveToPage("superuser-accounts");
-            //}
-          }
         }
       });
 
@@ -128,7 +156,23 @@ export class LoginComponent implements  OnInit {
 
   }
 
-
+  selectedRole(userdet) {    
+      this.result = true;
+      this.appService.user['roleName'] = userdet.roleName;
+      if (userdet.roleName == "CLIENT") {
+          this.appService.applicationViewMode = "Client";
+          this.appService.clientId = this.appService.user.userId;
+          this.appService.currentTab = 'clientview-petitions';
+          this.appService.moveToPage("clientview-petitions");
+      }
+      if (userdet.roleName == "IMMIGRATION MANAGER" || userdet.roleName == "IMMIGRATION OFFICER") {
+          this.appService.applicationViewMode = "Immigration";
+          this.appService.orgId = "2c9fc60d5c090bae015c0aab9958002b";
+          this.appService.currentTab = 'petitions';
+          this.appService.moveToPage("petitions");
+      }
+      this.close();
+  }
 
 
 }
