2e5a15207e1e60db6d7ad67d0a7775e817fffcef
vishnutelukutla <vishnutelukutla@gmail.com>
2017-07-11 18:24:34 +0530

    102,119 issues fixed

--- src/app/components/manageaccount-user-details/manageaccount-user-details.component.ts
+++ src/app/components/manageaccount-user-details/manageaccount-user-details.component.ts
@@ -9,9 +9,12 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 })
 export class ManageaccountUserDetailsComponent implements OnInit {
   private user: User;
-  public userList:any;
+  public userList: any = {};
   public orgList:any={};
-  public isUserEdit:boolean=true;
+  public isUserEdit: boolean = true;
+  public beforeCancelUserProfile: any;
+  public warningMessage: boolean = false;
+  public emailId: string;
   constructor( private appService: AppService,
         private route: ActivatedRoute) {
            if (this.appService.user) {
@@ -21,9 +24,30 @@ export class ManageaccountUserDetailsComponent implements OnInit {
   }
 
   ngOnInit() {
+    
     this.route.params.subscribe((res)=>{
       this.userList=res;
     })
   }
-
+  editProfileForm() {
+      this.beforeCancelUserProfile = (<any>Object).assign({}, this.userList);
+      this.isUserEdit = !this.isUserEdit;
+    
+  }
+  cancelProfileEdit() {
+      this.userList = this.beforeCancelUserProfile;
+      this.isUserEdit = !this.isUserEdit;
+  }
+  saveUserProfile() {
+      if (this.userList['firstName'] == '' || this.userList['firstName'] == null || this.userList['firstName'] == undefined
+          || this.userList['lastName'] == '' || this.userList['lastName'] == null || this.userList['lastName'] == undefined
+          || this.userList['role'] == '' || this.userList['role'] == null || this.userList['role'] == undefined
+          || this.userList['emailId'] == '' || this.userList['emailId'] == null || this.userList['emailId'] == undefined) {
+          this.warningMessage = true;
+      }
+      else {
+          this.warningMessage = false;
+          this.isUserEdit = true;
+      }
+  }
 }
