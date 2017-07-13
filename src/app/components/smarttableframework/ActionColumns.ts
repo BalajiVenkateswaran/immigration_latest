aca1e6b896c238571171a462f890868733a7c25b
balajiptg <balaji.venkateswaran@ptgindia.com>
2017-07-13 08:21:56 +0530

    Delete Icon File

---
+++ src/app/components/smarttableframework/ActionColumns.ts
@@ -0,0 +1,27 @@
+import {Component, OnDestroy, EventEmitter, Output,Input} from '@angular/core';
+import {ICellRendererAngularComp  } from 'ag-grid-angular/main';
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs';
+import { Subscription } from 'rxjs/Subscription';
+
+@Component({
+    selector: 'actions-columns',
+    template: `<span><i class="fa fa-trash" aria-hidden="true" (click)="delRow()"></i></span>`,
+
+
+})
+export class ActionColumns implements ICellRendererAngularComp    {
+    public params: any;
+    public static sendDeleteData = new Subject<boolean>();
+    agInit(params: any): void {
+        this.params = params;
+    }
+    
+    constructor(){
+       
+    }
+    delRow(){
+        ActionColumns.sendDeleteData.next(this.params.data);
+    }
+    
+}
