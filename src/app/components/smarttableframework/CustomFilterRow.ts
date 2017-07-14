322bffcc9d1e903e7ce6d7c1a0698a7a9853d490
balaji <balaji@PTGHYD>
2017-07-14 17:22:22 +0530

    Build Issue

--- src/app/components/smarttableframework/CustomFilterRow.ts
+++ src/app/components/smarttableframework/CustomFilterRow.ts
@@ -1,5 +1,4 @@
-﻿import { SmartTableFrameworkService } from './SmartTableFramework-service';
-import {Component, OnDestroy, EventEmitter, Output} from '@angular/core';
+﻿import {Component, OnDestroy, EventEmitter, Output} from '@angular/core';
 import {IHeaderAngularComp } from 'ag-grid-angular/main';
 import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs';
