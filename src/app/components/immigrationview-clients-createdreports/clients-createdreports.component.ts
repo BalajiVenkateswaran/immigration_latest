b62f4bc27b5c20ddc02abdc5808f62a63615173a
vishnutelukutla <vishnutelukutla@gmail.com>
2017-07-21 18:52:20 +0530

    build issue fixed

--- src/app/components/immigrationview-clients-createdreports/clients-createdreports.component.ts
+++ src/app/components/immigrationview-clients-createdreports/clients-createdreports.component.ts
@@ -28,6 +28,14 @@ export class clientscreatedreportscomponent implements OnInit {
     public barChartLabels: string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
     public barChartType: string = 'bar';
     public barChartLegend: boolean = true;
+    // events
+    public chartClicked(e: any): void {
+        console.log(e);
+    }
+
+    public chartHovered(e: any): void {
+        console.log(e);
+    }
 
     public barChartData: any[] = [
         { data: [65, 59, 80, 81, 56, 55, 40], label: 'Visa Approvals' },
