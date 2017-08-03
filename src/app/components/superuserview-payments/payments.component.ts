29a74825e1055c5181d9e924a6452888e9dac52f
vishnutelukutla <vishnutelukutla@gmail.com>
2017-08-03 15:13:45 +0530

    super user payments

--- src/app/components/superuserview-payments/payments.component.ts
+++ src/app/components/superuserview-payments/payments.component.ts
@@ -26,7 +26,7 @@ export interface ConfirmModel {
   templateUrl: './payments.component.html'
 })
 export class SuperUserViewPaymentstabComponent extends DialogComponent<ConfirmModel, boolean> implements OnInit {
-    private paymentsList: payment[];
+    
     public addClient: FormGroup; // our model driven form
     public submitted: boolean; // keep track on whether form is submitted
     private message: string;
@@ -38,98 +38,114 @@ export class SuperUserViewPaymentstabComponent extends DialogComponent<ConfirmMo
     public getClientsData: boolean = true;
     public newclitem: any = {};
     public DefaultResponse = {"status": "Active" };
-
-    settings = {
-        add: {
-            addButtonContent: '<i class="fa fa-plus-circle" aria-hidden="true"></i>',
-            createButtonContent: '<i class="fa fa-check" aria-hidden="true"></i>',
-            cancelButtonContent: '<i class="fa fa-times" aria-hidden="true"></i>',
-            confirmCreate: true
-        },
-        edit: {
-            editButtonContent: '<i class="fa fa-pencil" aria-hidden="true"></i>',
-            saveButtonContent: '<i class="fa fa-check" aria-hidden="true"></i>',
-            cancelButtonContent: '<i class="fa fa-times" aria-hidden="true"></i>',
-            confirmSave: true
-        },
-        actions: {
-            add: false,
-            edit: false,
-            delete: false
-        },
-        columns: {
-            paymentId: {
-                title: 'Payment Id'
-            },
-            accountName: {
-                title: 'Account Name'
-            },
-            accountNumber: {
-                title: 'Account Number'
-            },
-            invoiceNumber: {
-                title: 'Invoice Number'
-            },
-            invoiceDate: {
-                title: 'Invoice Date'
-            },
-            invoiceAmount: {
-                title: 'Invoice Amount'
-            },
-            paymentStatus: {
-                title: 'Payment Status'
-            },
-            paymentDate: {
-                title: 'Payment Date'
-            }
-        },
-        pager: {
-            display: true,
-            perPage: 10
-        }
-    };
+    public settings;
+    //settings = {
+    //    add: {
+    //        addButtonContent: '<i class="fa fa-plus-circle" aria-hidden="true"></i>',
+    //        createButtonContent: '<i class="fa fa-check" aria-hidden="true"></i>',
+    //        cancelButtonContent: '<i class="fa fa-times" aria-hidden="true"></i>',
+    //        confirmCreate: true
+    //    },
+    //    edit: {
+    //        editButtonContent: '<i class="fa fa-pencil" aria-hidden="true"></i>',
+    //        saveButtonContent: '<i class="fa fa-check" aria-hidden="true"></i>',
+    //        cancelButtonContent: '<i class="fa fa-times" aria-hidden="true"></i>',
+    //        confirmSave: true
+    //    },
+    //    actions: {
+    //        add: false,
+    //        edit: false,
+    //        delete: false
+    //    },
+    //    columns: {
+    //        paymentId: {
+    //            title: 'Payment Id'
+    //        },
+    //        accountName: {
+    //            title: 'Account Name'
+    //        },
+    //        accountNumber: {
+    //            title: 'Account Number'
+    //        },
+    //        invoiceNumber: {
+    //            title: 'Invoice Number'
+    //        },
+    //        invoiceDate: {
+    //            title: 'Invoice Date'
+    //        },
+    //        invoiceAmount: {
+    //            title: 'Invoice Amount'
+    //        },
+    //        paymentStatus: {
+    //            title: 'Payment Status'
+    //        },
+    //        paymentDate: {
+    //            title: 'Payment Date'
+    //        }
+    //    },
+    //    pager: {
+    //        display: true,
+    //        perPage: 10
+    //    }
+    //};
     //source: LocalDataSource = new LocalDataSource();
     constructor(private superuserviewPaymentstabService: SuperUserViewPaymentstabService, private appService: AppService, private router: Router, public dialogService: DialogService, private menuComponent: MenuComponent) {
         super(dialogService); 
-    }
-    source: LocalDataSource = new LocalDataSource();
-    getCliData() {
-        this.appService.showSideBarMenu(null, "clients");
-        this.superuserviewPaymentstabService.getPayments().subscribe((res) => {
-            this.paymentsList = res['payments'];
-            this.source.load(this.paymentsList);
+        this.settings = {
+            'isDeleteEnable': false,
+            'isAddButtonEnable': false,
+            'columnsettings': [
+                {
 
-            if (this.appService.user) {
-                this.user = this.appService.user;
-            }
-            });
+                    headerName: "Payment Id",
+                    field: "paymentId",
+                },
+                {
+
+                    headerName: "Account Name",
+                    field: "accountName",
+                },
+                {
+
+                    headerName: "Account Number",
+                    field: "accountNumber",
+
+                },
+                {
+                    headerName: "Invoice Number",
+                    field: "invoiceNumber",
+                },
+                {
+                    headerName: "Invoice Date",
+                    field: "invoiceDate",
+                },
+                {
+                    headerName: "Invoice Amount",
+                    field: "invoiceAmount",
+                },
+                {
+                    headerName: "Payment Status",
+                    field: "paymentStatus",
+                },
+                {
+                    headerName: "Payment Date",
+                    field: "paymentDate",
+                }
+            ]
+        }
     }
+
       ngOnInit() {
     this.appService.showSideBarMenu(null, "payments");
     this.superuserviewPaymentstabService.getPayments().subscribe((res: any) => {
-          this.paymentsList = res.payments;
-
-        this.source.load(this.paymentsList);
+        this.data = res.payments;
+       // this.source.load(this.paymentsList);
         });
     if (this.appService.user) {
         this.user = this.appService.user;
     }
   }
-    //addNewCli() {
-    //    this.dialogService.addDialog(SuperUserViewPaymentstabComponent, {
-    //        addNewClient: true,
-    //        getClientsData: false,
-    //        title: 'Add Client',
-    //    }).subscribe((isConfirmed) => {
-    //        if (isConfirmed) {
-    //            this.superuserviewPaymentstabService.saveNewClient(this.appService.newclitem).subscribe((res) => {
-    //                if (res['statusCode'] == 'SUCCESS') {
-    //                    this.getCliData();
-    //                }
-    //            });
-    //        }
-    //    });
-    //}
+    
     clientSave() {
         this.newclitem['accountId'] = this.appService.user.accountId;
         this.newclitem['orgId'] = this.appService.orgId;
@@ -205,7 +221,7 @@ export class SuperUserViewPaymentstabComponent extends DialogComponent<ConfirmMo
   //            }
   //        });
   //}
-  onUserRowClick(event): void{
+    editRecord(event): void{
       this.menuComponent.highlightSBLink('Payments');
       this.appService.moveToPage("accountdetails-payments");
     this.appService.clientId = event.data.clientId;
