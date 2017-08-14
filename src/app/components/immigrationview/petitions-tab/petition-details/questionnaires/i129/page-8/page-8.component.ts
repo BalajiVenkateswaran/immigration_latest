5838eaa79c6c0454f0d3e13350765c5c667f43b4
Surya Bhai <jsurya352@gmail.com>
2017-08-14 16:33:18 +0530

    page 8 integration

---
+++ src/app/components/immigrationview/petitions-tab/petition-details/questionnaires/i129/page-8/page-8.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {i129Page5Service} from "./page-5.service";
+import {AppService} from "../../../../../../../services/app.service";
+import {IMyOptions, IMyDateModel, IMyDate} from 'mydatepicker';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+    selector: 'app-page-5.component',
+    templateUrl: './page-5.component.html',
+    styleUrls: ['./page-5.component.scss']
+})
+export class i129Page5Component implements OnInit {
+
+     constructor() {
+    }
+
+    ngOnInit() {
+ 
+    }
+
+}
