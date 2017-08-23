a408e0c2d439bfc3cc732088c5a37672b3418d3b
balaji <balaji@PTGHYD>
2017-08-23 16:43:38 +0530

    Questionnaire I129DC Page2

--- src/app/components/immigrationview/petitions-tab/petition-details/questionnaires/i129DC/page-2/page-2.component.ts
+++ src/app/components/immigrationview/petitions-tab/petition-details/questionnaires/i129DC/page-2/page-2.component.ts
@@ -10,10 +10,72 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
     styleUrls: ['./page-2.component.scss']
 })
 export class i129dcPage2Component implements OnInit {
-
-     constructor() {
+    public questions;
+    public aptType;
+    public petitionType;
+    public page2:any={
+        "addressOfUSinstitution":{}
     }
+     constructor() {
+         this.questions = [
+            {
+                "id": "0",
+                "display": "Yes",
+                "value":"Y"
+            },
+            {
+                "id": "1",
+                "display": "No",
+                "value": "N"
+            },
+        ];
+         this.aptType = [
+            {
+                "id": "0",
+                "display": "Apt.",
+                "value":"APT"
+            },
+            {
+                "id": "1",
+                "display": "Stc.",
+                "value": "STE"
+            },
+            {
+                "id": "2",
+                "display": "Flr.",
+                "value": "FLR"
+            }
+            
+        ];
+        this.petitionType=[
+            {
+                "id":"0",
+                "display": "CAP H-1B Bachelor's Degree",
+                "value": "CAP_BACHELOR_DEGREE"
 
+            },
+             {
+                "id":"1",
+                "display": "CAP H-1B U.S. Master's Degree or Higher",
+                "value": "CAP_MASTER_DEGREE"
+
+            },
+             {
+                "id":"2",
+                "display": "CAP H-1B1 Chile/Singapore",
+                "value": "CAP_CHILE_SINGAPORE"
+
+            },
+             {
+                "id":"3",
+                "display": "CAP Exempt",
+                "value": "CAP_EXEMPT"
+
+            }
+        ]
+        
+    }
+    
     ngOnInit() {
  
     }
