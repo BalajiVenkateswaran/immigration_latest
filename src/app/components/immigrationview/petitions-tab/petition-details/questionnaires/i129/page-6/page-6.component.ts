4d079d40cf2ae064908f4e116c120ee659b237a7
balaji <balaji@PTGHYD>
2017-08-24 15:20:41 +0530

    Questionnaire I129 page 6,7,8 service integration

--- src/app/components/immigrationview/petitions-tab/petition-details/questionnaires/i129/page-6/page-6.component.ts
+++ src/app/components/immigrationview/petitions-tab/petition-details/questionnaires/i129/page-6/page-6.component.ts
@@ -3,6 +3,7 @@ import {i129Page6Service} from "./page-6.service";
 import {AppService} from "../../../../../../../services/app.service";
 import {IMyOptions, IMyDateModel, IMyDate} from 'mydatepicker';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { QuestionnaireCommonService } from '../../../questionnaires/common/questionnaire-common.service';
 
 @Component({
     selector: 'app-page-6.component',
@@ -10,12 +11,36 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
     styleUrls: ['./page-6.component.scss']
 })
 export class i129Page6Component implements OnInit {
-    public page6:any={};
-     constructor() {
+    beforecancelquestionnaire: any;
+    isquestionnaireEdit: boolean;
+    public page6: any = {};
+     constructor(public questionnaireService: QuestionnaireCommonService) {
     }
 
     ngOnInit() {
- 
+        this.isquestionnaireEdit = true;
+        this.questionnaireService.getQuestionnaireData("2c9fc60d5e0cef3a015e0f194da1006c", 6).subscribe(res => {
+            if (res['formPage'] != undefined) {
+                this.page6 = res['formPage'];
+            }
+        })
     }
+
+    editQuestinnaireForm() {
+        this.isquestionnaireEdit = !this.isquestionnaireEdit;
+        this.beforecancelquestionnaire = (<any>Object).assign({}, this.page6);
+    }
+    cancelQuestinnaireEdit() {
+        this.page6 = this.beforecancelquestionnaire;
+        this.isquestionnaireEdit = true;
+    }
+    savequestionnaireInformation() {
+        this.isquestionnaireEdit = true;
+        this.page6.pageNumber = 6;
+        this.questionnaireService.saveQuestionnaireData('2c9fc60d5e0cef3a015e0f194da1006c', 6, this.page6).subscribe(res => {
+
+        })
+    }
+
 
 }
