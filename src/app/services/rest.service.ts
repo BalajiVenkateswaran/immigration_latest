06561f6e1f4b84ba9f11012717beefc90c454974
PanindraVarma <panindra.rudraraju@ybranttech.com>
2017-10-17 14:25:10 -0400

    Angular upgrade to latest version and environment dev/prod setup

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -3,10 +3,12 @@ import {Headers, Http, RequestOptions, ResponseContentType} from "@angular/http"
 import {Observable} from 'rxjs/Observable';
 import '../rxjs-operators';
 import {HeaderService} from "../components/common/header/header.service";
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class RestService {
-      immp_endpoint_url: String = "http://34.200.77.115:8080/immigrationPortal";
+      immp_endpoint_url: String = environment.appUrl;
+      //"http://34.200.77.115:8080/immigrationPortal";
       // immp_endpoint_url: String = "http://localhost:8080/immigrationPortal";
 
   constructor(private http: Http, private headerService: HeaderService) {
