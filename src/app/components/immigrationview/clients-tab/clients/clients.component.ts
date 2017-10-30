33521b2a7d20e60c43c813339c78539121a7b9bb
vasudev baggi <vasudev.baggi@ptgindia.com>
2017-10-30 11:48:16 +0530

    Status-colmn-changes

--- src/app/components/immigrationview/clients-tab/clients/clients.component.ts
+++ src/app/components/immigrationview/clients-tab/clients/clients.component.ts
@@ -3,6 +3,7 @@ import { AppService } from '../../../../services/app.service';
 import { ConfirmComponent } from '../../../framework/confirmbox/confirm.component';
 import { HeaderService } from '../../../common/header/header.service';
 import { MenuComponent } from '../../../common/menu/menu.component';
+import { StatusButton } from './StatusButton';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ClientsService } from "./clients.service";
 import { FormGroup, FormControl } from "@angular/forms";
@@ -12,229 +13,229 @@ import {SortType} from "../../../framework/smarttable/types/query-parameters";
 
 
 export interface ConfirmModel {
-  title: string;
-  message: string;
-  addNewClient: boolean;
-  addMorefilters: boolean;
-  getClientsData: boolean;
+    title: string;
+    message: string;
+    addNewClient: boolean;
+    addMorefilters: boolean;
+    getClientsData: boolean;
 }
 
 
 @Component({
-  selector: 'app-clients',
-  templateUrl: './clients.component.html',
-  styleUrls: ['./clients.component.sass']
+    selector: 'app-clients',
+    templateUrl: './clients.component.html',
+    styleUrls: ['./clients.component.sass']
 })
 export class ClientsComponent extends DialogComponent<ConfirmModel, boolean> implements OnInit {
-  public queryParams: any;
-  private message: string;
-  private clientName: any;
-  public addNewClient: boolean;
-  public addMorefilters: boolean;
-  public getClientsData: boolean = true;
-  public newclitem: any = {};
-  public warningMessage: boolean = false;
-  public settings;
-  public data;
-  public paginationData;
-  constructor(private clientService: ClientsService, private appService: AppService,
-    public dialogService: DialogService, private menuComponent: MenuComponent,
-    private headerService: HeaderService) {
-    super(dialogService);
+    public queryParams: any;
+    private message: string;
+    private clientName: any;
+    public addNewClient: boolean;
+    public addMorefilters: boolean;
+    public getClientsData: boolean = true;
+    public newclitem: any = {};
+    public warningMessage: boolean = false;
+    public settings;
+    public data;
+    public paginationData;
+    constructor(private clientService: ClientsService, private appService: AppService,
+        public dialogService: DialogService, private menuComponent: MenuComponent,
+        private headerService: HeaderService) {
+        super(dialogService);
 
-    this.settings = {
-      'columnFilter': false,
-      'isDeleteEnable': false,
-      'customPanel': true,
-      'isAddFilterButtonEnable':true,
-      'defaultFilter': [{
-          headingName: "status",
-          headerName: "Status",
-          filterValue: "Active"
-        }
-      ],
-      filter : {
-        quick : [
-          {
-            headerName : 'Status',
-            field : 'status',
-            values : [
-              {alias : 'Active', value : 'Active'},
-              {alias : 'Inactive', value : 'Inactive'}
+        this.settings = {
+            'columnFilter': false,
+            'isDeleteEnable': false,
+            'customPanel': true,
+            'isAddFilterButtonEnable': true,
+            'defaultFilter': [{
+                headingName: "status",
+                headerName: "Status",
+                filterValue: "Active"
+            }
+            ],
+            filter: {
+                quick: [
+                    {
+                        headerName: 'Status',
+                        field: 'status',
+                        values: [
+                            { alias: 'Active', value: 'Active' },
+                            { alias: 'Inactive', value: 'Inactive' }
+                        ]
+                    }
+                ]
+            },
+            'sort': [{
+                headingName: "lastUpdate",
+                sort: SortType.DESC
+            }],
+            'columnsettings': [
+                {
+                    headerName: "First Name",
+                    field: "firstName"
+                },
+                {
+                    headerName: "Last Name",
+                    field: "lastName"
+                },
+                {
+                    headerName: "Email Address",
+                    field: "email"
+                },
+                {
+                    headerName: "Phone",
+                    field: "phone"
+                },
+                {
+                    headerName: "Status",
+                    cellRendererFramework: StatusButton
+                },
+                {
+                    headerName: "Open Petitions",
+                    field: "openPetitions"
+                }
             ]
-          }
-        ]
-      },
-      'sort' : [{
-        headingName: "lastUpdate",
-        sort: SortType.DESC
-      }],
-      'columnsettings': [
-        {
-          headerName: "First Name",
-          field: "firstName"
-        },
-        {
-          headerName: "Last Name",
-          field: "lastName"
-        },
-        {
-          headerName: "Email Address",
-          field: "email"
-        },
-        {
-          headerName: "Phone",
-          field: "phone"
-        },
-        {
-          headerName: "Status",
-          field: "status"
-        },
-        {
-          headerName: "Open Petitions",
-          field: "openPetitions"
+
         }
-        ]
+    }
+
+
+    ngOnInit() {
+        this.headerService.showSideBarMenu(null, "clients");
+        this.headerService.showSideBarMenu(null, "clients");
+    }
+    getClients() {
 
     }
-  }
+    filteradd() {
+        console.log("Filter Add");
+        this.dialogService.addDialog(ClientsComponent, {
+            addMorefilters: true,
+            getClientsData: false,
+            title: 'Add Client',
+        }).subscribe((isConfirmed) => {
+            if (isConfirmed) {
+                //this.clientService.saveNewClient(this.appService.newclitem).subscribe((res) => {
+                //    if (res['statusCode'] == 'SUCCESS') {
+                //        this.clientService.getClients(this.queryParams, this.headerService.selectedOrg['orgId']).subscribe(
+                //            (res) => {
+                //                this.data = res['clients'];
+                //                this.paginationData = res['pageMetadata'];
+
+                //            }
+                //        )
+                //    }
+                //    if (res['statusDescription'] == 'Duplicate client') {
+                //        this.dialogService.addDialog(ConfirmComponent, {
+                //            title: 'Error..!',
+                //            message: 'User already Exists'
+                //        })
+                //    }
+                //});
+            }
+        });
+    }
+
+    addNewCli() {
+        this.dialogService.addDialog(ClientsComponent, {
+            addNewClient: true,
+            getClientsData: false,
+            title: 'Add Client',
+        }).subscribe((isConfirmed) => {
+            if (isConfirmed) {
+                this.clientService.saveNewClient(this.appService.newclitem).subscribe((res) => {
+                    if (res['statusCode'] == 'SUCCESS') {
+                        this.clientService.getClients(this.queryParams, this.headerService.selectedOrg['orgId']).subscribe(
+                            (res) => {
+                                this.data = res['clients'];
+                                this.paginationData = res['pageMetadata'];
+
+                            }
+                        )
+                    }
+                    if (res['statusDescription'] == 'Duplicate client') {
+                        this.dialogService.addDialog(ConfirmComponent, {
+                            title: 'Error..!',
+                            message: 'User already Exists'
+                        })
+                    }
+                });
+            }
+        });
+    }
+    capitialize(data) {
+        if (this.newclitem['firstName']) {
+            this.newclitem['firstName'] = data['firstName'].split(' ').map(item => { return item.charAt(0).toUpperCase() + item.slice(1) }).join(' ');
+        }
+        if (this.newclitem['lastName']) {
+            this.newclitem['lastName'] = data['lastName'].split(' ').map(item => { return item.charAt(0).toUpperCase() + item.slice(1) }).join(' ');
+        }
+    }
+    clientSave(email, phone) {
+        this.newclitem['accountId'] = this.headerService.user.accountId;
+        this.newclitem['orgId'] = this.headerService.selectedOrg['orgId'];
+        this.newclitem['createdBy'] = this.headerService.user.userId;
+        if (this.newclitem['status'] == '' || null || undefined) {
+            this.newclitem['status'] = "Active";
+        }
+
+        if (this.newclitem['firstName'] == '' || this.newclitem['firstName'] == null || this.newclitem['firstName'] == undefined || this.newclitem['lastName'] == '' || this.newclitem['lastName'] == null || this.newclitem['lastName'] == undefined || this.newclitem['email'] == '' || this.newclitem['email'] == null || this.newclitem['email'] == undefined || this.newclitem['phone'] == '' || this.newclitem['phone'] == null || this.newclitem['phone'] == undefined) {
+            this.warningMessage = true;
+        }
+        else if (email != null || phone != null) {
+            this.warningMessage;
+        }
+        else {
+            this.warningMessage = false;
+            this.appService.newclitem = this.newclitem;
+            this.result = true;
+            this.close();
+        }
 
 
-  ngOnInit() {
-    this.headerService.showSideBarMenu(null, "clients");
-    this.headerService.showSideBarMenu(null, "clients");
-  }
-  getClients(){
+    }
+    cancel() {
+        this.result = false;
+        this.close();
+    }
 
-  }
-  filteradd() {
-    console.log("Filter Add");
-      this.dialogService.addDialog(ClientsComponent, {
-          addMorefilters: true,
-          getClientsData: false,
-          title: 'Add Client',
-      }).subscribe((isConfirmed) => {
-          if (isConfirmed) {
-              //this.clientService.saveNewClient(this.appService.newclitem).subscribe((res) => {
-              //    if (res['statusCode'] == 'SUCCESS') {
-              //        this.clientService.getClients(this.queryParams, this.headerService.selectedOrg['orgId']).subscribe(
-              //            (res) => {
-              //                this.data = res['clients'];
-              //                this.paginationData = res['pageMetadata'];
 
-              //            }
-              //        )
-              //    }
-              //    if (res['statusDescription'] == 'Duplicate client') {
-              //        this.dialogService.addDialog(ConfirmComponent, {
-              //            title: 'Error..!',
-              //            message: 'User already Exists'
-              //        })
-              //    }
-              //});
-          }
-      });
-  }
 
-  addNewCli() {
-    this.dialogService.addDialog(ClientsComponent, {
-      addNewClient: true,
-      getClientsData: false,
-      title: 'Add Client',
-    }).subscribe((isConfirmed) => {
-      if (isConfirmed) {
-        this.clientService.saveNewClient(this.appService.newclitem).subscribe((res) => {
-          if (res['statusCode'] == 'SUCCESS') {
-            this.clientService.getClients(this.queryParams,this.headerService.selectedOrg['orgId']).subscribe(
-              (res)=>{
+    onDeleteConfirm(clients) {
+        this.clientName = clients.data.firstName;
+        this.dialogService.addDialog(ConfirmComponent, {
+            title: 'Confirmation',
+            message: 'Are you sure you want to Delete ' + this.clientName + ' ?'
+        })
+            .subscribe((isConfirmed) => {
+                //Get dialog result
+                if (isConfirmed) {
+                    this.clientService.removeclient(clients.data['clientId'], this.headerService.user.userId).subscribe((res) => {
+                        this.message = res['statusCode'];
+                        clients.data.clientStatus = "Mark for Deletion";
+                        clients.confirm.reject();
+                    });
+
+                }
+            });
+    }
+    onUserRowClick(event): void {
+        this.menuComponent.highlightSBLink('Client Details');
+        this.appService.moveToPage("immigrationview-client-details");
+        this.appService.clientId = event.data.clientId;
+
+    }
+    dataWithParameters(queryData) {
+        if (queryData) {
+            this.queryParams = queryData;
+        }
+        this.clientService.getClientsWithQueryParams(this.headerService.selectedOrg['orgId'], queryData).subscribe(
+            res => {
                 this.data = res['clients'];
                 this.paginationData = res['pageMetadata'];
-
-              }
-            )
-          }
-          if (res['statusDescription'] == 'Duplicate client') {
-            this.dialogService.addDialog(ConfirmComponent, {
-              title: 'Error..!',
-              message: 'User already Exists'
             })
-          }
-        });
-      }
-    });
-  }
-  capitialize(data){
-    if(this.newclitem['firstName']){
-      this.newclitem['firstName']=data['firstName'].split(' ').map(item=>{return item.charAt(0).toUpperCase()+item.slice(1)}).join(' ');
     }
-    if(this.newclitem['lastName']){
-      this.newclitem['lastName']=data['lastName'].split(' ').map(item=>{return item.charAt(0).toUpperCase()+item.slice(1)}).join(' ');
-    }
-  }
-  clientSave(email, phone) {
-    this.newclitem['accountId'] = this.headerService.user.accountId;
-    this.newclitem['orgId'] = this.headerService.selectedOrg['orgId'];
-    this.newclitem['createdBy'] = this.headerService.user.userId;
-    if (this.newclitem['status'] == '' || null || undefined) {
-      this.newclitem['status'] = "Active";
-    }
-
-    if (this.newclitem['firstName'] == '' || this.newclitem['firstName'] == null || this.newclitem['firstName'] == undefined || this.newclitem['lastName'] == '' || this.newclitem['lastName'] == null || this.newclitem['lastName'] == undefined || this.newclitem['email'] == '' || this.newclitem['email'] == null || this.newclitem['email'] == undefined || this.newclitem['phone'] == '' || this.newclitem['phone'] == null || this.newclitem['phone'] == undefined) {
-      this.warningMessage = true;
-    }
-    else if (email != null || phone != null) {
-      this.warningMessage;
-    }
-    else {
-      this.warningMessage = false;
-      this.appService.newclitem = this.newclitem;
-      this.result = true;
-      this.close();
-    }
-
-
-  }
-  cancel() {
-    this.result = false;
-    this.close();
-  }
-
-
-
-  onDeleteConfirm(clients) {
-    this.clientName = clients.data.firstName;
-    this.dialogService.addDialog(ConfirmComponent, {
-      title: 'Confirmation',
-      message: 'Are you sure you want to Delete ' + this.clientName + ' ?'
-    })
-      .subscribe((isConfirmed) => {
-        //Get dialog result
-        if (isConfirmed) {
-          this.clientService.removeclient(clients.data['clientId'], this.headerService.user.userId).subscribe((res) => {
-            this.message = res['statusCode'];
-            clients.data.clientStatus = "Mark for Deletion";
-            clients.confirm.reject();
-          });
-
-        }
-      });
-  }
-  onUserRowClick(event): void {
-    this.menuComponent.highlightSBLink('Client Details');
-    this.appService.moveToPage("immigrationview-client-details");
-    this.appService.clientId = event.data.clientId;
-
-  }
-  dataWithParameters(queryData) {
-    if(queryData){
-      this.queryParams=queryData;
-    }
-    this.clientService.getClientsWithQueryParams(this.headerService.selectedOrg['orgId'], queryData).subscribe(
-      res => {
-        this.data = res['clients'];
-        this.paginationData = res['pageMetadata'];
-      })
-  }
 
 
 }
---
+++ src/app/components/immigrationview/clients-tab/clients/statusButton.ts
@@ -0,0 +1,29 @@
+﻿import { AppService } from '../../../../services/app.service';
+import { Component, OnDestroy} from '@angular/core';
+import { ICellRendererAngularComp } from 'ag-grid-angular/main';
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs';
+import { Subscription } from 'rxjs/Subscription';
+import {HeaderService} from "../../../common/header/header.service";
+
+@Component({
+    template: `
+    <div>
+     <button class="iportal-btn" style="background: #27AE60;">Active</button>
+     <button class="iportal-btn"style="color:#757575">Inactive</button>
+    </div>
+   `
+
+})
+export class StatusButton implements ICellRendererAngularComp {
+    public params: any;
+    public switchdisable: boolean;
+    constructor(public appService: AppService, public headerService: HeaderService) {
+    }
+    agInit(params: any): void {
+        this.params = params;
+    }
+    refresh(): boolean {
+        return false;
+    }
+}
