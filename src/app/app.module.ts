33d542a5f6945bdbabff2851565cfadb45e5857e
Surya Bhai <jsurya352@gmail.com>
2017-10-31 17:43:04 +0530

    metirial design implemented for form pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -301,9 +301,47 @@ import {FilterComponent} from './components/framework/smarttable/filter/filter.c
 
 
 
+//
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatStepperModule,
+} from '@angular/material';
+import {CdkTableModule} from '@angular/cdk/table';
 
 @NgModule({
     imports: [
+        MatInputModule,
+        BrowserAnimationsModule,
         BrowserModule,
         AngularDraggableModule,
         ChartsModule,
