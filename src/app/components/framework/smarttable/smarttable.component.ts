fd7ee22691c57e32c01fa4c49816f562c38fa304
vasudev baggi <vasudev.baggi@ptgindia.com>
2017-11-08 15:19:33 +0530

    Clientview-changes

--- src/app/components/framework/smarttable/smarttable.component.ts
+++ src/app/components/framework/smarttable/smarttable.component.ts
@@ -283,7 +283,7 @@ export class SmartTableFrameworkComponent extends DialogComponent<ConfirmModel, 
         if (this.settings.hasOwnProperty('rowHeight')) {
             this.gridOptions['rowHeight'] = this.settings['rowHeight'];
         } else {
-            this.gridOptions['rowHeight'] = 60;
+            this.gridOptions['rowHeight'] = 55;
         }
         this.gridOptions['columnDefs'] = this.settings['columnsettings'];
     }
