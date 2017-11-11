f52a41a0fddaebb8c45428f1aa5a47111044ee35
vasudev baggi <vasudevbaggi@outlook.com>
2017-11-11 10:11:15 +0530

    Grid-scrollbar-allignments-Fixed

--- src/app/components/framework/smarttable/smarttable.component.ts
+++ src/app/components/framework/smarttable/smarttable.component.ts
@@ -270,7 +270,7 @@ export class SmartTableFrameworkComponent extends DialogComponent<ConfirmModel, 
         if (this.settings.hasOwnProperty('rowHeight')) {
             this.gridOptions['rowHeight'] = this.settings['rowHeight'];
         } else {
-            this.gridOptions['rowHeight'] = 55;
+            this.gridOptions['rowHeight'] = 35;
         }
         this.gridOptions['columnDefs'] = this.settings['columnsettings'];
     }
--- src/app/components/immigrationview/clients-tab/clients/statusButton.ts
+++ src/app/components/immigrationview/clients-tab/clients/statusButton.ts
@@ -1,11 +1,11 @@
-﻿import { Component} from '@angular/core';
+import { Component} from '@angular/core';
 import { ICellRendererAngularComp } from 'ag-grid-angular/main';
 
 @Component({
     template: `
     <div>
-     <button class="iportal-btn" *ngIf="params.data.status==='Active'" style="background: #27AE60;">Active</button>
-     <button class="iportal-btn" *ngIf="params.data.status==='Inactive'" style="color:#757575">Inactive</button>
+     <span *ngIf="params.data.status==='Active'" style="background: #27AE60;border-radius: 4px;padding: 2px 3px 3px 5px; cursor: none;">Active</span>
+     <span  *ngIf="params.data.status==='Inactive'">Inactive</span>
     </div>
    `
 })
