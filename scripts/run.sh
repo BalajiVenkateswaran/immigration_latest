5e880e6ce6262ad333a3d8beb8fb40aac8ec47c1
PanindraVarma <panindra.rudraraju@ybranttech.com>
2017-12-07 22:07:37 -0500

    Http to Https redirect

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -x
 
+echo "<VirtualHost *:80>
+   DocumentRoot /var/www/html
+   ServerName www.dev-theimmigrationhub.com
+   RewriteEngine On
+   RewriteCond %{HTTP:X-Forwarded-Proto} =http
+   RewriteRule . https://%{HTTP:Host}%{REQUEST_URI} [L,R=permanent]
+</VirtualHost>" >> /etc/httpd/conf/httpd.conf
 echo 'inside ui run script'
 service httpd restart
